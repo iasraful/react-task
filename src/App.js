69763094d59340309391784f957f258e2a43752f
Asraful Islam <iasraful321@gmail.com>
2021-06-27 15:36:59 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Components/Header/Header";
 import CardDetails from "./Components/CardDetalis/CardDetails";
