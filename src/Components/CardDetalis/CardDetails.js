17e958c4b8978d45793db2f6c5a00e35e5a12d6b
Asraful Islam <iasraful321@gmail.com>
2021-06-27 13:23:57 +0600

    fixed the bug

--- src/Components/CardDetalis/CardDetails.js
+++ src/Components/CardDetalis/CardDetails.js
@@ -4,33 +4,31 @@ import { useParams } from "react-router-dom";
 const CardDetails = () => {
   const { name } = useParams();
   const [details, setDetails] = useState([]);
-  const {capital, topLevelDomain, subregion, flag, population, area } = details;
+  const { capital, timezones, topLevelDomain, region, flag, population, area } = details;
   useEffect(() => {
     const url = `https://restcountries.eu/rest/v2/name/${name}`;
     fetch(url)
       .then((res) => res.json())
-      .then((data) => setDetails(data));
+      .then((data) => setDetails(data[0]));
   }, [name]);
-console.log(details);
   return (
-      <div className="container">
-
-    {/* <div className="row">
+    <div className="container">
+      <div className="row">
       <div className="col-md-12">
         <div className="card bg-dark text-white">
           <img src={flag} className="card-img" alt="..." />
           <div className="card-img-overlay">
-            <h3 className="card-title">Capital: {capital}</h3>
-            <p className="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-            <p className="card-text">Last updated 3 mins ago</p>
+            <h1 className="card-title"> {name}</h1>
+            <h5 className="card-title">Capital: {capital}</h5>
+            <h5 className="card-title">Region: {region}</h5>
+            <h5 className="card-title">Time-Zone: {timezones}</h5>
+            <h5 className="card-title">Population: {population}</h5>
+            <h5 className="card-title">Area: {area}</h5>
+            <p className="card-text">Top Domain:{topLevelDomain}</p>
           </div>
         </div>
       </div>
-    </div> */}
+    </div>
     </div>
   );
 };
